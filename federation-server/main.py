a4c0e53e7e02feada12109670c80fd837a11a433
donzelli <donzelli@c86f3312-a118-4c92-335e-7b446ece7ebd>
2011-02-23 08:07:47 +0000

    Version that we used for the CONET meeting in Bonn 23-02-2011

--- federation-server/main.py
+++ federation-server/main.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from django.utils import simplejson as json
 from odict import OrderedDict
 
@@ -10,32 +11,52 @@ from google.appengine.ext import db
 
 # DATASTORE MODEL
 
-class Federation(db.Model):
-    uri = db.StringProperty()
-    media_type = db.StringProperty()
-    name = db.StringProperty()
-    # testbeds = db.ListProperty()
-    # jobs = db.ListProperty()
+# class Federation(db.Model):
+#     uri = db.StringProperty()
+#     media_type = db.StringProperty()
+#     name = db.StringProperty()
+#     # testbeds = db.ListProperty()
+#     # jobs = db.ListProperty()
     
 class Testbed(db.Model):
-    uri = db.StringProperty()
-    media_type = db.StringProperty()
     name = db.StringProperty()
-    organiziation = db.StringProperty()
+    organization = db.StringProperty()
     server_url = db.StringProperty()
     
-class Platform(db.Model):
-    uri = db.StringProperty()
-    media_type = db.StringProperty()
-    name = db.StringProperty()
+    def to_dict(self, head_only = False):
+        testbed = OrderedDict()
+        testbed['uri'] = 'https://federation-server.appspot.com/testbeds/' + str(self.key().id())
+        testbed['media_type'] = 'application/json'
+        testbed['name'] = self.name
+        if not head_only:
+            testbed['organization'] = self.organization
+            testbed['server_url'] = self.server_url + '/'
+            # testbed['platforms'] = testbed['uri'] + '/platforms/'
+            # testbed['jobs'] = testbed['uri'] + '/jobs/'
+        return testbed
     
-class Job(db.Model):
-    name = db.StringProperty()
-    uri = db.StringProperty()
-    media_type = db.StringProperty()
-    # testbed = db.ReferenceProperty(Testbed)
-    datetime_from = db.DateTimeProperty()
-    datetime_to   = db.DateTimeProperty()
+# class Platform(db.Model):
+#     uri = db.StringProperty()
+#     media_type = db.StringProperty()
+#     name = db.StringProperty()
+    
+# class Job(db.Model):
+#     name = db.StringProperty()
+#     # testbed = db.ReferenceProperty(Testbed)
+#     datetime_from = db.DateTimeProperty()
+#     datetime_to   = db.DateTimeProperty()
+#     
+#     def to_dict(self):
+#         job = OrderedDict()
+#         job['uri'] = 'https://federation-server.appspot.com/jobs/' + str(self.key().id())
+#         job['media_type'] = 'application/json'
+#         job['name'] = self.name
+#         job['organization'] = self.organization
+#         job['server_url'] = self.server_url
+#         job['platforms'] = job['uri'] + '/platforms/'
+#         job['jobs'] = job['uri'] + '/jobs/'
+#         return job
+        
     
 # DATASTORE INITIALIZATION
     
@@ -46,43 +67,47 @@ class DatastoreInitialization(webapp.RequestHandler):
             t.delete()
 
         t1 = Testbed()
-        t1.uri = 'https://federation-server.appspot.com/testbeds/1'
-        t1.media_type = 'application/json'
         t1.name = 'TWIST'
-        t1.organiziation = 'TU Berlin'
+        t1.organization = 'TU Berlin'
         t1.server_url = 'https://www.twist.tu-berlin.de:8001'
         t1.put()
+        
+        t2 = Testbed()
+        t2.name = 'Other Testbed'
+        t2.organization = 'Other Organization'
+        t2.server_url = 'https://www.example.org'
+        t2.put()
         self.response.out.write('Datastore has been initialized!')
 
 
 # TASK EXAMPLE
 
-class TasksHandler(webapp.RequestHandler):
-    def post(self):
-        taskqueue.add(
-            method = 'POST',
-            url = '/reflector',
-            params = {
-                'method' : 'PUT',
-                'url' : 'http://127.0.0.1:8081/nodegroups/123',
-            }
-        )
-        self.response.set_status(201)
-        self.response.out.write('New Task created at <a href="http://localhost:8080/tasks/456">http://localhost:8080/tasks/456</a>\n')
-    
-class TaskHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.set_status(200)
-        self.response.out.write('Task ID = 456, Method = PUT, URL = http://localhost:8000/nodegroups/123\n')
-
-class Reflector(webapp.RequestHandler):
-    def post(self):
-        # logging.info(self.request.get('method'))
-        # logging.info(self.request.get('url'))
-        result = urlfetch.fetch(
-            method = self.request.get('method'),
-            url = self.request.get('url'),
-        )
+# class TasksHandler(webapp.RequestHandler):
+#     def post(self):
+#         taskqueue.add(
+#             method = 'POST',
+#             url = '/reflector',
+#             params = {
+#                 'method' : 'PUT',
+#                 'url' : 'http://127.0.0.1:8081/nodegroups/123',
+#             }
+#         )
+#         self.response.set_status(201)
+#         self.response.out.write('New Task created at <a href="http://localhost:8080/tasks/456">http://localhost:8080/tasks/456</a>\n')
+#     
+# class TaskHandler(webapp.RequestHandler):
+#     def get(self):
+#         self.response.set_status(200)
+#         self.response.out.write('Task ID = 456, Method = PUT, URL = http://localhost:8000/nodegroups/123\n')
+# 
+# class Reflector(webapp.RequestHandler):
+#     def post(self):
+#         # logging.info(self.request.get('method'))
+#         # logging.info(self.request.get('url'))
+#         result = urlfetch.fetch(
+#             method = self.request.get('method'),
+#             url = self.request.get('url'),
+#         )
     
 # CONET DEMO
 
@@ -100,25 +125,32 @@ class FederationResourceHandler(webapp.RequestHandler):
         
 class TestbedCollectionHandler(webapp.RequestHandler):
     def get(self):
-        pass
-        # self.response.out.write(json.dumps(testbeds))
+        testbed_collection_list = list()
+        for testbed in Testbed.all():
+            testbed_collection_list.append(testbed.to_dict(head_only = True))
+        self.response.out.write(json.dumps(testbed_collection_list))
         
-    
-class JobCollectionHandler(webapp.RequestHandler):
-    def get(self):
-        result = urlfetch.fetch(
-            method = 'GET',
-            url = 'http://127.0.0.1:8000/jobs/',
-            validate_certificate = None
-        )
-        self.response.set_status(200)
-        self.response.out.write(result.content)
+class TestbedResourceHandler(webapp.RequestHandler):
+    def get(self, id):
+        testbed = Testbed.get_by_id(int(id))
+        self.response.out.write(json.dumps(testbed.to_dict(head_only = False)))
+
+# class JobCollectionHandler(webapp.RequestHandler):
+#     def get(self):
+#         result = urlfetch.fetch(
+#             method = 'GET',
+#             url = 'http://127.0.0.1:8000/jobs/',
+#             validate_certificate = None
+#         )
+#         self.response.set_status(200)
+#         self.response.out.write(result.content)
         
 class TestbedJobCollectionHandler(webapp.RequestHandler):
-    def get(self):
+    def get(self, id, slug):
         result = urlfetch.fetch(
             method = 'GET',
-            url = 'https://www.twist.tu-berlin.de:8001/jobs/',
+            url = Testbed.get_by_id(int(id)).server_url + '/jobs/',
+            validate_certificate = False
         )
         self.response.set_status(200)
         self.response.out.write(result.content)
@@ -129,15 +161,21 @@ def main():
         # ('/tasks/', TasksHandler),
         # ('/tasks/456', TaskHandler),
         # ('/reflector', Reflector),
-
-        ('/',                   FederationResourceHandler),
-        # ('/testbeds/',        TestbedCollectionHandler),
-        # ('/testbeds/(.*)',    TestbedResourceHandler),
-        # ('/jobs/',            JobCollectionHandler),
-        # ('/jobs/(.*)',        JobResourceHandler),
         
-        ('/testbeds/1/jobs/',   TestbedJobCollectionHandler),
-        ('/datastore-initialization/',   DatastoreInitialization),
+        (r'^/$', FederationResourceHandler),
+        (r'^/testbeds/$', TestbedCollectionHandler),
+
+        (r'^/testbeds/(.*)/(.*)/$', TestbedJobCollectionHandler),
+        (r'^/testbeds/(.*)$', TestbedResourceHandler),
+        
+        
+        
+        
+        
+        # (r'/jobs/', JobCollectionHandler),
+        # (r'/jobs/(.*)', JobResourceHandler),
+        
+        (r'^/datastore-initialization/$',   DatastoreInitialization),
     ], debug=True)
     util.run_wsgi_app(application)
 
