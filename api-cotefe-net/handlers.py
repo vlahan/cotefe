6edda7b708683b42d4f7bfdf59367e3b83dd4fa4
solenoidd <solenoidd@c86f3312-a118-4c92-335e-7b446ece7ebd>
2012-02-13 02:59:18 +0000

    experiment definition completed

--- api-cotefe-net/handlers.py
+++ api-cotefe-net/handlers.py
@@ -1279,8 +1279,6 @@ class VirtualNodeGroupCollectionHandler(OAuth2RESTJSONHandler):
         vng.owner = self.user
         vng.experiment = experiment
         vng.put()
-        
-        # TODO: now create connection which each of the virtual nodes
 
         for vn_id in vng_dict['virtual_nodes']:
             VirtualNodeGroup2VirtualNode(vng = vng, vn = VirtualNode.get_by_id(int(vn_id))).put()
@@ -1308,10 +1306,55 @@ class VirtualNodeGroupResourceHandler(OAuth2RESTJSONHandler):
 # VIRTUAL TASK
 
 class VirtualTaskCollectionHandler(OAuth2RESTJSONHandler):
-    pass
+    
+    def options(self):
+        allowed_methods = ['GET', 'POST']
+        OAuth2RESTJSONHandler.options(self, allowed_methods)
+    
+    def get(self, experiment_id):
+        
+        experiment = Experiment.get_by_id(int(experiment_id))
+        
+        virtual_task_list = list()
+        query = VirtualTask.all().filter('experiment =', experiment)
+        for virtual_task in query:
+            virtual_task_list.append(virtual_task.to_dict(head_only = True))
+        self.response.out.write(serialize(virtual_task_list))
+            
+    def post(self, experiment_id):
+        
+        experiment = Experiment.get_by_id(int(experiment_id))
+        
+        vt_dict = json.loads(self.request.body)
+        
+        vt = VirtualTask()
+        vt.name = vt_dict['name']
+        vt.description = vt_dict['description']
+        vt.method = vt_dict['method']
+        vt.target = vt_dict['target']
+        vt.owner = self.user
+        vt.experiment = experiment
+        vt.put()
+        
+        self.response.status = '201'
+        self.response.headers['Location'] = '%s' % vt.uri()
+        self.response.headers['Content-Location'] = '%s' % vt.uri()
 
 class VirtualTaskResourceHandler(OAuth2RESTJSONHandler):
-    pass
+    
+    def options(self):
+        allowed_methods = ['GET', 'DELETE']
+        OAuth2RESTJSONHandler.options(self, allowed_methods)
+    
+    def get(self, experiment_id, vt_id):
+        
+        vt = VirtualTask.get_by_id(int(vt_id))
+        self.response.out.write(serialize(vt.to_dict()))
+            
+    def delete(self, experiment_id, virtual_nodegroup_id):
+        
+        vt = VirtualTask.get_by_id(int(vt_id))
+        vt.delete()
     
 # JOB
 
