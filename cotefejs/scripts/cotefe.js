faf312e03aec9f8d409978bda5a6c2736507184d
sanjeet.raj@gmail.com <sanjeet.raj@gmail.com@c86f3312-a118-4c92-335e-7b446ece7ebd>
2012-04-25 12:24:23 +0000

    user resource available

--- cotefejs/scripts/cotefe.js
+++ cotefejs/scripts/cotefe.js
@@ -1,4 +1,90 @@
 (function($) {
+
+/*
+ * cotefe constants
+ */
+	var cotefe = {
+			baseurl:"http://localhost:8080",
+			apiurl: "https://api.cotefe.net/",
+			token : "1908700504b2474e9e9d6cc6225d3ece",
+			
+		};
+
+	user = Backbone.Model.extend({
+			//id:"https://api.cotefe.net/me?access_token=1908700504b2474e9e9d6cc6225d3ece",
+			url:cotefe.apiurl+"me"+"?access_token="+cotefe.token,
+	 		display: function() {
+		    this.fetch({
+		      	success: function(model, response) {
+		        		console.log(response);
+		        		console.log("***************************");
+		        		console.log(JSON.stringify(model));
+		        		//console.log(model.parse(response));
+		      		}
+	    		});
+	  		}
+	});
+
+	project= Backbone.Model.extend({
+		url:cotefe.apiurl+"projects/?access_token="+cotefe.token,
+		display: function() {
+		    	this.fetch({
+		      		success: function(model, response) {
+		        			//console.log(response);
+		        			console.log("***************************");
+		        			console.log(JSON.stringify(model));
+		        			projectsview=new projectsview({model:model});
+		        			projectsview.render();
+		      			}
+	    			});
+	  			}	
+		
+	});
+	
+	projectsview= Backbone.View.extend({
+		el:"#projects",
+		render:function(eventname)
+			{
+				$(this.el).append(JSON.stringify(this.model));
+			}
+		
+	});
+	
+	/*
+	 * event for 
+	 */
+	clickOp= function()
+	{
+		$(".get").click(function(event){alert(event)});
+	}
+	
+})(jQuery);
+
+$(document).ready(function(){
+   //model = new Model({label:'Test label'});
+   //view = new ListAll({model: model});
+   
+   
+   res= new user();
+   res.display();
+   clickOp();
+   projects= new project();
+   projects.display();
+  
+   
+	
+	
+	/*
+	 * slide form divs
+	 */
+	$("h3").click(function(){$(this).next("form").slideToggle("slow");});
+
+   
+ });
+
+
+
+/*
 //////test
 Model = Backbone.Model.extend();
 ListAll= Backbone.View.extend({
@@ -12,40 +98,5 @@ ListAll= Backbone.View.extend({
   	
 });
 
-
-
-resource= Backbone.Model.extend({
-		id:"https://api.cotefe.net/me?access_token=1908700504b2474e9e9d6cc6225d3ece",
-		url:"https://api.cotefe.net/me?access_token=1908700504b2474e9e9d6cc6225d3ece",
- 		display: function() {
-	    this.fetch({
-	      success: function(model, response) {
-	        console.log(response);
-	        console.log("***************************");
-	        console.log(model.get('first'));
-	      }
-    });
-  }
-});
-
-
-})(jQuery);
-
-$(document).ready(function(){
-   model = new Model({label:'Test label'});
-   view = new ListAll({model: model});
-   
-   
-   res= new resource();
-   res.display();
-
-	
-	
-	
-	/*
-	 * slide form divs
-	 */
-	$("h3").click(function(){$(this).next("form").slideToggle("slow");});
-
-   
- });
+/////// testing place
+*/
---
+++ cotefejs/scripts/cotefe1.js
@@ -0,0 +1,146 @@
+(function($){
+	
+	var cotefe = {
+			baseurl:"http://localhost:8080",
+			apiurl: "https://api.cotefe.net/",
+			token : "1908700504b2474e9e9d6cc6225d3ece",
+			
+		};
+	
+	/*
+	 * models
+	 */
+	userInfo = Backbone.Model.extend({
+				url:cotefe.apiurl+"me"+"?access_token="+cotefe.token,
+				uri:"",
+				media_type:"",
+				username:"",
+				id: 31034,
+				first: "Sanjeet Raj",
+				last: "Pandey",
+				email: "sanjeet.raj@gmail.com",
+				organization: "TKN",
+				projects:[],
+				experiemnts:[],
+				jobs: [],
+				datetime_created: "2012-02-03T17:59:46+0000",
+				datetime_modified: "2012-04-18T09:37:48+0000",
+				
+				display: function() {
+				    this.fetch({
+				      	success: function(model, response) {
+				      			new UserView({model:model});
+				        		return (model);
+				      		}
+			    		});
+			  	},
+				
+				
+	      });
+	
+	project  = Backbone.Model.extend({
+				url:cotefe.apiurl+"projects/?access_token="+cotefe.token,
+				uri: "https://api.cotefe.net/projects/47001",
+				media_type: "application/json",
+				name: "Project1",
+				id: 47001,
+				description: "hey there fdagra",
+				owner: {},
+				experiments:[],
+				experiment_count: 1,
+				datetime_created: "2012-04-18T09:47:14+0000",
+				datetime_modified: "2012-04-18T09:47:24+0000",
+				display: function() {
+				    this.fetch({
+				      	success: function(model, response) {
+				      			
+				        		return (model);
+				      		}
+			    		});
+			  		}
+		  });
+	
+	experiemnt=Backbone.Model.extend({
+				url:cotefe.apiurl+"experiments/?access_token="+cotefe.token,
+				uri: "https://api.cotefe.net/experiments/45002",
+				media_type: "application/json",
+				name: "Experiment1",
+				id: 45002,
+				description: "Experiment1",
+				owner: {},
+				project: {},
+				images: [],
+				property_sets: [],
+				virtual_nodes: [],
+				virtual_node_count: 0,
+				virtual_node_groups: [],
+				virtual_tasks: [],
+				jobs: [],
+				datetime_created: "2012-04-18T09:47:54+0000",
+				datetime_modified: "2012-04-18T09:47:54+0000",
+				display: function() {
+				    this.fetch({
+				      	success: function(model, response) {
+				        		return (model);
+				      		}
+			    		});
+			  		}
+		 });
+	
+	
+	
+	
+	/*
+	 *
+	 */
+	
+	/*
+	 * template
+	 */
+	
+	
+	UserView = Backbone.View.extend({		
+		el: "#userContent",
+		
+		template:'',
+		
+		events:{
+			"click #clickme":"loaduser",
+		},
+			
+		initialize : function() {  _.bindAll(this, "render");this.render(); },
+		render : function() 
+		{
+	    		
+	    		$(this.el).append( _.template('<textarea>'+(JSON.stringify(this.model,null,'\t'))+' </textarea>',this.model.toJSON()));
+	    		return this;
+	   },
+	  	
+	  	loaduser:function(){alert(JSON.stringify(this.model));}
+	});
+	
+	 
+	 
+
+})(jQuery)
+$(document).ready(function(){
+   
+   user= new userInfo();
+   user.display();
+   proj= new project();
+   proj.display();
+   exp= new experiemnt();
+   exp.display();
+   
+   
+  
+   
+	
+	
+	/*
+	 * slide form DIV
+	 */
+	$("h3").on("click",function(){$(this).next("form").slideToggle("slow");});
+
+   
+ });
